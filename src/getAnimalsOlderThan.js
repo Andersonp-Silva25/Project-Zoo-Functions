df57efbd399fee1334dbe11f9bf351792783fb39
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-06-29 19:29:23 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = species.filter((specie) => specie.name === animal);
+  const [residents] = animals;
+  const animalAges = residents.residents;
+  return animalAges.every((animalAge) => animalAge.age >= age);
 }
+
+// console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
