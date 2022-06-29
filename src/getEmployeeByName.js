17c656db933329dd9a12c0d4d4877e7fbacff9ec
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-06-29 20:22:45 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  function filtro(elemento) {
+    return elemento.firstName.includes(employeeName) || elemento.lastName.includes(employeeName);
+  }
+  return employees.find(filtro);
 }
 
 module.exports = getEmployeeByName;
