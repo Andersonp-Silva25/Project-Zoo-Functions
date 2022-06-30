f0ebef06a3eb2d97a6846a4946d0964693969cf9
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-06-30 18:47:37 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((entrant) => entrant.age < 18).length;
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const seniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child: childs, adult: adults, senior: seniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === [] || entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const people = countEntrants(entrants);
+  const totalChild = (people.child * prices.child);
+  const totalAdult = (people.adult * prices.adult);
+  const totalSenior = (people.senior * prices.senior);
+  return totalAdult + totalChild + totalSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
