9ba9c313f6016dd0cc946ab8ef8ef1bbe46641fe
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-06-30 14:25:42 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(...animal) {
+  const allSpecies = species.filter((specie) => specie).map(({ residents }) => residents.length);
+  const animalNames = species.map(({ name }) => name);
+  const joinArray = animalNames.map((animalName, index) => ({ [animalName]: allSpecies[index] }));
+
+  if (animal.length === 0) return Object.assign(...joinArray);
+
+  const parameters = Object.values(...animal);
+  const speciesName = species.find((specie) => parameters.includes(specie.name));
+  const speciesSex = speciesName.residents.filter((resident) => parameters.includes(resident.sex));
+
+  if (parameters.includes('female') || parameters.includes('male')) return speciesSex.length;
+  if (speciesName) return speciesName.residents.length;
+  return animal;
 }
 
 module.exports = countAnimals;
