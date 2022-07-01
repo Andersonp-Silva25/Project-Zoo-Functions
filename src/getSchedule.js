a065ddc280a553454f18c742125f6f110b8639d6
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-07-01 18:22:03 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
+const verifyAnimal = (animal) => {
+  const verifyType = species.find((specie) => specie.name === animal);
+  return verifyType.availability;
+};
+
+const hoursDay = (day) => {
+  if (hours[day].open === 0) return 'CLOSED';
+  return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+};
+
+const availabilityAnimal = (day) => {
+  if (day === 'Monday') return 'The zoo will be closed!';
+  return species.filter((specie) => specie.availability.includes(day)).map((animal) => animal.name);
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const availabilityDays = {};
+  Object.keys(hours).forEach((day) => {
+    availabilityDays[[day]] = {
+      officeHour: hoursDay(day),
+      exhibition: availabilityAnimal(day),
+    };
+  });
+  const isAnimal = species.some((specie) => specie.name === scheduleTarget);
+  const isDay = availabilityDays[scheduleTarget];
+  if (isAnimal) return verifyAnimal(scheduleTarget);
+  if (isDay) {
+    const objDay = {};
+    const verifyDay = Object.keys(hours).filter((day) => day === scheduleTarget).join();
+    objDay[verifyDay] = availabilityDays[scheduleTarget];
+    return objDay;
+  }
+  return availabilityDays;
 }
 
 module.exports = getSchedule;
