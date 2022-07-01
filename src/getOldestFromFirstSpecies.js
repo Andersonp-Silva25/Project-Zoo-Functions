33f2514b4d8a0f18919a4e9df2785fb0021796c0
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-07-01 19:03:29 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeId = employees.find((employee) => employee.id === id);
+  const firstSpecie = employeeId.responsibleFor.find((first) => first);
+  const allAnimails = species.find((specie) => specie.id === firstSpecie).residents;
+  const ages = allAnimails.map((count) => count.age);
+  const animalOldest = ages.reduce((acc, curr) => (acc > curr ? acc : curr));
+  return Object.values(allAnimails.find((animal) => animal.age === animalOldest));
 }
 
 module.exports = getOldestFromFirstSpecies;
