02bad94b9d0e90d5e2b001850ff5ab25617a64d2
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-07-01 23:13:53 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const verifyParameter = (parameter) => {
+  const verifyEmployee = Object.keys(parameter).join();
+  const verifyValue = Object.values(parameter).join();
+  if (verifyEmployee === 'name') {
+    const person = employees
+      .find(({ firstName, lastName }) => firstName === verifyValue || lastName === verifyValue);
+    return person;
+  }
+  if (verifyEmployee === 'id') {
+    const person = employees
+      .find(({ id }) => id === verifyValue);
+    return person;
+  }
+};
+
+const parameterUndefined = () => {
+  const employeeId = employees.filter((employee) => employee);
+  const mapa = employeeId.map((employee) => {
+    const animals = verifyParameter({ id: employee.id }).responsibleFor;
+    const verifyAnimalId = species.filter(({ id }) => animals.includes(id));
+    const animalName = verifyAnimalId.map((animal) => animal.name);
+    const places = verifyAnimalId.map((animal) => animal.location);
+    return {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: animalName,
+      locations: places,
+    };
+  });
+  return mapa;
+};
+
+function getEmployeesCoverage(parameter) {
+  if (parameter !== undefined) {
+    if (verifyParameter(parameter) === undefined) throw new Error('Informações inválidas');
+    const animals = verifyParameter(parameter).responsibleFor;
+    const verifyAnimalId = species.filter(({ id }) => animals.includes(id));
+    const animalName = verifyAnimalId.map((animal) => animal.name);
+    const places = verifyAnimalId.map((animal) => animal.location);
+    const obj = {
+      id: verifyParameter(parameter).id,
+      fullName: `${verifyParameter(parameter).firstName} ${verifyParameter(parameter).lastName}`,
+      species: animalName,
+      locations: places,
+    };
+    return obj;
+  }
+  return parameterUndefined();
 }
 
 module.exports = getEmployeesCoverage;
