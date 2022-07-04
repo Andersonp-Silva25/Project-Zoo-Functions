80f0ce4a80ed35b2aed5d215e66392114765664a
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-07-04 18:04:44 -0300

    Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const includeNameTrue = (options) => {
+  const obj = { NE: [], NW: [], SE: [], SW: [] };
+  const { sex, sorted } = options;
+  species.forEach(({ name, residents, location }) => {
+    const objAnimals = {};
+    let verifySex;
+    if (sex) {
+      verifySex = residents.filter((resident) => sex === resident.sex)
+        .map((resident) => resident.name);
+    } else {
+      verifySex = residents.map((resident) => resident.name);
+    }
+    objAnimals[name] = verifySex;
+    if (sorted) objAnimals[name].sort();
+    obj[location].push(objAnimals);
+  });
+  return obj;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || !options.includeNames) {
+    const obj = { NE: [], NW: [], SE: [], SW: [] };
+    species.forEach(({ location, name }) => obj[location].push(name));
+    return obj;
+  }
+  if (options.includeNames) return includeNameTrue(options);
 }
 
 module.exports = getAnimalMap;
